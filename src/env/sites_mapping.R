95190d0be765535f1818a3d3eac26ac79d8e785c
Camille <rumberger.c@northeastern.edu>
2026-01-18 15:19:04 -0500

    file rename

---
+++ src/env/sites_mapping.R
@@ -0,0 +1,124 @@
+############################
+## MAPPING SEASCAPE SITES ##
+############################
+
+library(ggplot2)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+library(ggrepel)
+
+setwd("~/Documents/Github/MVP_oyster_GO")
+
+seascape_sites <- read.csv("data/EnvDat/seascape/SeascapeSamples_site.csv")
+exp_pops <- read.csv("data/IndDat/popsdf.csv")[,-1]
+exp_pops$region <- c("South","South","South","Local","Local","Local","North","North","Exp","Exp")
+exp_pops$ancestral_group <- c("Gulf","Gulf","Atlantic","Atlantic","Selected","Selected","Atlantic","Atlantic","Exp","Exp")
+exp_pops$site_name_plotting <- gsub("_", "-", exp_pops$site_name)
+exp_pops[exp_pops$site_name_plotting == "LEWISETTA",]$site_name_plotting <- "Lewisetta"
+exp_pops[exp_pops$site_name_plotting == "YORKRIVER",]$site_name_plotting <- "York River"
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+world_crop <- sf::st_crop(world, c(xmin =-100 , xmax = -60, ymin = 24, ymax = 50))
+coastline <- ne_coastline(scale = "medium", returnclass = "sf")
+
+ches_ext <- st_bbox(c(xmin =-77.856476 , xmax = -72.762787, ymin = 36.019557, ymax = 39.939163),
+                    crs = st_crs(world))
+world_chesapeake <- sf::st_crop(world, ches_ext)
+coastline_chesapeake <- sf::st_crop(coastline, ches_ext)
+
+seascape_map_basic <- ggplot(data = coastline) +
+  geom_sf() +
+  theme_classic() +
+  labs(title = "Map of Seascape Sampling Sites",
+       x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(-100, -60), ylim = c(24, 50), expand = FALSE) +
+  geom_point(data = seascape_sites, aes(x = Longitude_manual, y = Latitude_manual),
+             color = "black", fill = "#3155A9", shape = 21, size = 3)
+seascape_map_basic
+
+exp_sites_basic <- ggplot(data = coastline) + 
+  geom_sf()+
+  theme_classic()+
+  labs(title = "Map of Experimental Adult Source Sites",
+       x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(-100, -60), ylim = c(24, 50), expand = FALSE) +
+  geom_point(data = exp_pops[1:8,], aes(x = longitude, y = latitude),
+             color = "black", fill  = "orange", shape = 21, size = 3)+
+  geom_label_repel(data = exp_pops[1:8,],
+                   aes(x = longitude, y = latitude, label = site_name),
+                   nudge_x = -3,
+                   nudge_y = 0.5,
+                   fill = "white")
+exp_sites_basic
+
+# putting the whole thing together
+seascape_exp_map <- ggplot(data = coastline) +
+  geom_sf() +
+  theme_classic() +
+  labs(title = "Map of Seascape & Experimental Population \nSource Sites",
+       x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(-100, -60), ylim = c(24, 50), expand = FALSE) +
+  geom_rect(aes(xmin = -77.856476, xmax = -72.762787, ymin = 36.019557, ymax = 39.939163),
+            color = "black",
+            fill = "transparent",
+            size = 0.5) +
+  geom_point(data = seascape_sites, aes(x = Longitude_manual, y = Latitude_manual),
+             color = "black", fill = "#3155A9", shape = 21, size = 3) +
+  geom_point(data = exp_pops[1:8,], aes(x = longitude, y = latitude),
+             color = "black", fill  = "orange", shape = 22, size = 3)+
+  geom_label_repel(data = exp_pops[exp_pops$pop %in% c("TX", "LA", "FL", "NH", "ME"),],
+                   aes(x = longitude, y = latitude, label = site_name_plotting),
+                   min.segment.length = 0,
+                   fill = "white")
+seascape_exp_map
+
+# and a common garden inset panel
+common_gardens <- ggplot(data = coastline_chesapeake) + 
+  theme_classic() +
+  geom_sf() +
+  labs(title = "Map of Common Garden Sites",
+       x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(-77.856476, -72.762787), ylim = c(36.019557, 39.939163), expand = FALSE) +
+  geom_point(data = exp_pops[9:10,], aes(x = longitude-0.15, y = latitude, fill = site_name_plotting),
+             color = "black", shape = 24, size = 4, show.legend = FALSE)+
+  scale_fill_manual(values = c("#00fa9a","#1d90ff"),
+                    breaks = c("Lewisetta","York River")) +
+  geom_point(data = exp_pops[exp_pops$pop %in% c("VA","LOLA","DEBY"),], aes(x = longitude, y = latitude),
+             color = "black", fill  = "orange", shape = 22, size = 3)+
+  geom_point(data = seascape_sites[seascape_sites$Latitude_manual <= 39.939163 & 
+                                     seascape_sites$Latitude_manual >= 36.019557 & 
+                                     seascape_sites$Longitude_manual <= -72.762787 & 
+                                     seascape_sites$Longitude_manual >= -77.856476,], aes(x = Longitude_manual, y = Latitude_manual),
+             color = "black", fill = "#3155A9", shape = 21, size = 3) +
+  geom_label_repel(data = exp_pops[exp_pops$pop %in% c("DEBY","LOLA"),],
+                   aes(x = longitude, y = latitude, label = site_name_plotting),
+                   min.segment.length = 0,
+                   nudge_x = 1.7,
+                   nudge_y = 0.3,
+                   fill = "white")+
+  geom_label_repel(data = exp_pops[exp_pops$pop %in% c("LEWISETTA","YORKRIVER"),],
+                   aes(x = longitude-0.15, y = latitude, label = site_name_plotting),
+                   nudge_x = -0.5,
+                   nudge_y = 0.3,
+                   min.segment.length = 0,
+                   fill = "white") +
+  geom_label_repel(data = exp_pops[exp_pops$pop %in% c("VA"),],
+                   aes(x = longitude, y = latitude, label = site_name_plotting),
+                   nudge_x = 0.5,
+                   nudge_y = -0.7,
+                   min.segment.length = 0,
+                   fill = "white") +
+  theme(legend.position = "none")
+common_gardens
+
+# export to ppt
+library(officer)
+library(rvg)
+
+doc <- read_pptx() %>% 
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with(value = dml(ggobj = seascape_exp_map), location = ph_location_type(type = "body")) %>%
+  add_slide(layout = "Title and Content", master = "Office Theme") %>% 
+  ph_with(value = dml(ggobj = common_gardens), location = ph_location_type(type = "body"))
+print(doc, target = "results/figures/site_maps.pptx")
